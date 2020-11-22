1a9683097bd8d10bb4191a84b0c08129a9f471d0
jacox02 <jacobleandro@icloud.com>
2020-11-22 13:00:33 -0400

    Se anadio Axios para las peticiones a API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
+import React,{ Component  } from "react";
 import './App.css';
+const axios = require('axios').default; 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  getVideos(){
+    axios.get('https://jsonplaceholder.typicode.com/todos/1')
+         .then(response => console.log(response.data))
+  }
+  render(){
+  return(<h1>{this.getVideos()}</h1>)
+  }
 }
 
 export default App;
